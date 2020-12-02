87d578128dcbe2da881d773cbc28732b9b514353
ihollander <ianholla@gmail.com>
2020-12-02 15:07:32 -0500

    Added solution

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,15 @@
-// Code DigitalClicker Component Here
+import React, { useState } from 'react'
+
+function DigitalClicker() {
+  const [timesClicked, setTimesClicked] = useState(0);
+
+  function handleClick() {
+    setTimesClicked(prevTimesClicked => prevTimesClicked + 1)
+  }
+
+  return (
+    <button onClick={handleClick}>{timesClicked}</button>
+  )
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,43 @@
-// Code YouTubeDebugger Component Here
+import React, { useState } from 'react'
+
+function YouTubeDebugger(props) {
+  const [settings, setSettings] = useState({
+    bitrate: 8,
+    video: {
+      resolution: '1080p'
+    }
+  })
+
+  function handleBitrateClick() {
+    setSettings(prevSettings => {
+      return {
+        ...prevSettings,
+        bitrate: 12
+      }
+    })
+  }
+
+  function handleResolutionClick() {
+    setSettings(prevSettings => {
+      return {
+        ...prevSettings,
+        video: {
+          resolution: "720p"
+        }
+      }
+    })
+  }
+
+  return (
+    <div>
+      <button className="bitrate" onClick={handleBitrateClick}>
+        Current bitrate: {settings.bitrate}
+      </button>
+      <button className="resolution" onClick={handleResolutionClick}>
+        Current resolution: {settings.video.resolution}
+      </button>
+    </div>
+  )
+}
+
+export default YouTubeDebugger
