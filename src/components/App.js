c4ee7d607cbdd57e605848d6246ef32062f00d02
Liz Burton <liz.burton147@gmail.com>
2021-08-17 10:40:27 -0400

    Update starter code to removed unused state

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-  const [items, setItems] = useState(itemData);
-
-  // this data will be passed down to the ShoppingList as a prop
-  console.log(items);
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
@@ -18,7 +14,7 @@ function App() {
         <h2>Shopster</h2>
         <button>Dark Mode</button>
       </header>
-      <ShoppingList items={items} />
+      <ShoppingList items={itemData} />
     </div>
   );
 }
