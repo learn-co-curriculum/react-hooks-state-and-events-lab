8c2afd0e84e61f5fe1f4cb78971c650d16e6a98b
Macolister <bispo.mac@gmail.com>
2023-05-24 20:17:19 -0400

    add dark mode functionality with state variable. listen for click on btn

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+ //use state here
+ const [darkModeOn, setDarKMode] = useState(false);
+
+  function DarkMode(){
+      //what use state does
+      console.log("I've been clicked")
+      setDarKMode((darkModeOn) => !darkModeOn) 
+      //console.log(darkModeOn) 
+  }
+  //console.log(darkModeOn) 
+
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = darkModeOn ? `App dark` : `App light`
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick = {DarkMode}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
