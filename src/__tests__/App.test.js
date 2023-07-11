251a3ed9fdff173c3d3d9d24adb77d05f2702fce
sean <balayans2014@gmail.com>
2023-07-11 11:40:01 -1000

    fixes test query selectors to search for App light and App dark class instead of light and dark class in document

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,27 +4,27 @@ import App from "../components/App";
 
 test("displays in 'light' mode when initialized", () => {
   const { container } = render(<App />);
-  expect(container.querySelector(".light")).toBeInTheDocument();
+  expect(container.querySelector(".App.light")).toBeInTheDocument();
 });
 
 test("changes to 'dark' mode when the button is clicked", () => {
   const { container } = render(<App />);
-  expect(container.querySelector(".light")).toBeInTheDocument();
+  expect(container.querySelector(".App.light")).toBeInTheDocument();
 
   fireEvent.click(screen.getByText(/ Mode/));
 
-  expect(container.querySelector(".dark")).toBeInTheDocument();
+  expect(container.querySelector(".App.dark")).toBeInTheDocument();
 });
 
 test("changes back to 'light' mode when the button is clicked twice", () => {
   const { container } = render(<App />);
-  expect(container.querySelector(".light")).toBeInTheDocument();
+  expect(container.querySelector(".App.light")).toBeInTheDocument();
 
   fireEvent.click(screen.getByText(/ Mode/));
 
-  expect(container.querySelector(".dark")).toBeInTheDocument();
+  expect(container.querySelector(".App.dark")).toBeInTheDocument();
 
   fireEvent.click(screen.getByText(/ Mode/));
 
-  expect(container.querySelector(".light")).toBeInTheDocument();
+  expect(container.querySelector(".App.light")).toBeInTheDocument();
 });
